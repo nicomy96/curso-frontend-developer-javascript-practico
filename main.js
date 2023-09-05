f21cabefcb305b33ae1f338848af701c195e2619
elnegativo <nicomy96@gmail.com>
2023-09-05 13:26:24 -0500

    Agregado carrito de compras

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside)
 
 function toggleDesktopMenu(){
+    closeIfOpen(aside);
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive')
+    closeIfOpen(aside);
+    mobileMenu.classList.toggle('inactive');
+}
+function toggleCarritoAside(){
+    closeIfOpen(mobileMenu);
+    closeIfOpen(desktopMenu);
+    aside.classList.toggle('inactive');
+
+}
+function closeIfOpen(element){
+    const isElementClose = element.classList.contains('inactive');
+    if(!isElementClose){
+        element.classList.add('inactive');
+    }
 }
